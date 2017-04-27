60d63e52098fb19f4cd6061630e739bb44dd177e
frol <frol.mr@ya.ru>
2017-04-27 14:50:03 +0300

    deploy environment is ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+posts = [
+  { title: 'test_1', body: 'body_test_1', username: 'vasya' },
+  { title: 'test_2', body: 'body_test_2', username: 'petya' },
+  { title: 'test_3', body: 'body_test_3', username: 'kolya' }
+]
+
+posts.each do |post|
+  Post.create(post)
+end
